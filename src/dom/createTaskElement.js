c0c6eff0b0b890e8cb6b71d5c2e9e57455c79c2c
N3mEee <n3mebusiness@gmail.com>
2023-01-14 22:33:32 +0100

    add style for tasks

--- src/dom/createTaskElement.js
+++ src/dom/createTaskElement.js
@@ -5,30 +5,42 @@ export default function createTaskElement(item) {
     task.classList.add("task");
     container.appendChild(task);
 
+    const taskDetails = document.createElement("div");
+    taskDetails.className = "task-details";
+    task.appendChild(taskDetails);
+
     const checkBox = document.createElement("input");
     checkBox.setAttribute("type", "checkbox");
     item.checked ? (checkBox.checked = true) : (checkBox.checked = false);
-    task.appendChild(checkBox);
+    taskDetails.appendChild(checkBox);
+
+    // const detailsContainer = document.createElement("div");
+    // detailsContainer.className = "details-container";
+    // taskDetails.appendChild(detailsContainer);
 
     const title = document.createElement("div");
     title.classList.add("task-title");
     title.textContent = item.titleValue;
-    task.appendChild(title);
+    taskDetails.appendChild(title);
 
     const date = document.createElement("div");
     date.classList.add("task-date");
     date.textContent = item.dateValue;
-    task.appendChild(date);
+    taskDetails.appendChild(date);
 
     const priority = document.createElement("div");
     priority.classList.add("task-priority");
     priority.textContent = item.priority;
-    task.appendChild(priority);
+    taskDetails.appendChild(priority);
+
+    const taskIcons = document.createElement("div");
+    taskIcons.className = "task-icons";
+    task.appendChild(taskIcons);
 
     //view
     const viewTask = document.createElement("div");
     viewTask.classList.add("view-task-btn");
-    task.appendChild(viewTask);
+    taskIcons.appendChild(viewTask);
 
     const viewTaskIcon = document.createElement("i");
     viewTaskIcon.className = "fa-solid fa-eye";
@@ -37,7 +49,7 @@ export default function createTaskElement(item) {
     //edit
     const editTask = document.createElement("div");
     editTask.classList.add("view-task-btn");
-    task.appendChild(editTask);
+    taskIcons.appendChild(editTask);
 
     const editTaskIcon = document.createElement("i");
     editTaskIcon.className = "fa-solid fa-pen-to-square";
@@ -46,7 +58,7 @@ export default function createTaskElement(item) {
     //delete
     const deleteTaskBtn = document.createElement("div");
     deleteTaskBtn.classList.add("delete-task-btn");
-    task.appendChild(deleteTaskBtn);
+    taskIcons.appendChild(deleteTaskBtn);
 
     const deleteTaskIcon = document.createElement("i");
     deleteTaskIcon.className = "fa-solid fa-trash";
